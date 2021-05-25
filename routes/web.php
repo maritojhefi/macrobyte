530b03f85c329862aa9198ba657ac37645d7c6ba
Mario <maritojhefi@outlook.com>
2021-05-25 14:05:40 -0400

    nueva vista google verificacion

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/google085c423aaa790337', function () {
+    return view('asd');
+});
