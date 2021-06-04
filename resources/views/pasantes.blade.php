9e27d7066bbe477dbc5be321618216a218ae72fd
Mario <maritojhefi@outlook.com>
2021-06-03 21:03:26 -0400

    formulario pasantes 1.4

--- resources/views/pasantes.blade.php
+++ resources/views/pasantes.blade.php
@@ -49,7 +49,7 @@
                                         @csrf
                                         <h3>Datos personales</h3>
                                         <section>
-                                            <h5 class="card-title">Datos personales</h5>
+                                            <h5 class="card-title">Esta informacion es confidencial</h5>
                                             
                                             <div class="form-group">
                                                 <label for="userName">Nombres y Apellidos</label>
@@ -75,7 +75,7 @@
                                             <div class="form-group">
                                                 
                                                 <div class="file-upload">
-                                                    <label for="confirm">Foto frontal actual</label>
+                                                    <label for="confirm">Foto actual del rostro</label>
                                                     <label for="subir" class="btn btn-success m-b-0 m-l-5 m-r-5 btn-rounded btn-outline"  data-toggle="tooltip" data-placement="top" data-original-title="Subir una nueva foto"><span class="material-icons">file_upload</span></label>
                                                     <input id="subir" class="form-control file-upload__input required" type="file" name="imagen">
                                                 </div>
@@ -147,7 +147,7 @@
                                             <h5 class="card-title">Informacion Importante</h5>
                                             <small>Macrobyte es una empresa joven de desarrollo software, seguridad informatica, redes y telecomunicaciones en pleno crecimiento con sede en Tarija, luego de enviar este formulario, en un plazo maximo de 4 dias, en caso de haber sido seleccionad@, nos pondremos en contacto contigo para coordinar la entrevista personal. Gracias por tu tiempo y exitos en tu carrera profesional!</small>
                                             <br>
-                                            <button type="submit" class="btn btn-success btn-rounded btn-sm">Enviar</button>
+                                            <button type="submit" class="btn btn-success btn-sm btn-block">Enviar formulario</button>
 
                                         </section>
                                     </form>
