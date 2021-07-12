6c879f7e87e9af7de093facd769ab6a3d1b57dd0
Mario <maritojhefi@outlook.com>
2021-07-12 15:23:57 -0400

    qr.1

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -8,7 +8,10 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 class QrController extends Controller
 {
     public function index(){
-        return QrCode::format('png')->merge('http://w3adda.com/wp-content/uploads/2019/07/laravel.png', 0.3, true)
+        $image = QrCode::format('png')->merge('http://w3adda.com/wp-content/uploads/2019/07/laravel.png', 0.3, true)
             ->size(200)->errorCorrection('H')
-            ->generate('W3Adda Laravel Tutorial');    }
+            ->generate('W3Adda Laravel Tutorial');
+return response($image)->header('Content-type','image/png');  
+        
+        }
 }
