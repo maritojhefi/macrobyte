62b5af0123ba96fe90d016d413a1bbd4e97ac819
unknown <maritojhefi@gmail.com>
2022-06-07 17:26:14 -0400

    circuito 2.0

--- database/seeders/ColaWhatsappSeeder.php
+++ database/seeders/ColaWhatsappSeeder.php
@@ -17,7 +17,7 @@ class ColaWhatsappSeeder extends Seeder
         ColaWhatsapp::truncate();
         ColaWhatsapp::create([
             'user_id'=>3,
-            'atendiendo'=>1,
+            'atendiendo'=>0,
            
         ]);
         ColaWhatsapp::create([
